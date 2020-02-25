ff99e3e52632163eedd898e71b1df0b4e612905a
PhaserArray <me@phaserarray.me>
2020-02-25 17:20:30 +0200

    implement visual toggling & basic i18n

--- extension/background/moodle-controller.js
+++ extension/background/moodle-controller.js
@@ -1,23 +1,23 @@
-browser.runtime.onInstalled.addListener(async function(){
-    console.log("WIP plugin did something, yay!");
-    // const registeredScript = await browser.contentScripts.register({
-    //     js: [{
-    //         file: 'content/moodle-tweaker.js'
-    //     }],
-    //     matches: [
-    //         'https://*/*'
-    //     ]
-    // });
-});
+// browser.runtime.onInstalled.addListener(async function(){
+//     console.log("WIP plugin did something, yay!");
+//     // const registeredScript = await browser.contentScripts.register({
+//     //     js: [{
+//     //         file: 'content/moodle-tweaker.js'
+//     //     }],
+//     //     matches: [
+//     //         'https://*/*'
+//     //     ]
+//     // });
+// });
 
-browser.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    console.log(tabId);
-    console.log(changeInfo);
-    console.log(tab);
-    $.get(tab.url.match(/^https?:\/\/[^/]+/)[0] + "/lib/upgrade.txt", function(data){
-        console.log(data);
-    });
-    browser.tabs.executeScript(tabId, {
-        code: "console.log('test')"
-    });
-});
+// browser.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+//     console.log(tabId);
+//     console.log(changeInfo);
+//     console.log(tab);
+//     $.get(tab.url.match(/^https?:\/\/[^/]+/)[0] + "/lib/upgrade.txt", function(data){
+//         console.log(data);
+//     });
+//     browser.tabs.executeScript(tabId, {
+//         code: "console.log('test')"
+//     });
+// });
--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,1 +1,31 @@
 console.log("Popup script!")
+
+function optionToggled(event) {
+    console.log(event);
+    let element = (event.target.tagName == "BUTTON") ? event.target : event.target.parentElement;
+    let enabled = element.classList.contains("enabled");
+    if (enabled) {
+        // disable
+    } else {
+        // enable
+    }
+    element.classList.toggle("enabled"); // reimplement above
+}
+
+function localizePopupHTML() {
+    document.querySelectorAll("[data-localize]").forEach(element => {
+        console.log(element.getAttribute("data-localize"));
+        console.log(browser.i18n.getMessage(element.getAttribute("data-localize")));
+        element.innerHTML = browser.i18n.getMessage(element.getAttribute("data-localize"));
+    });
+    document.querySelectorAll("[data-localize-title]").forEach(element => {
+        element.setAttribute("title", browser.i18n.getMessage(element.getAttribute("data-localize-title")));
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    localizePopupHTML();
+    document.querySelectorAll("#settings>button").forEach(element => {
+        element.addEventListener("click", optionToggled);
+    });
+});
