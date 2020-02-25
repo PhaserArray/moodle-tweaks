08a6a456cee7e1a185aaf20a69aca0af75e3326d
PhaserArray <me@phaserarray.me>
2020-02-25 19:32:05 +0200

    Implement popup side changing&updating of options

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,15 +1,66 @@
 console.log("Popup script!")
 
 function optionToggled(event) {
-    console.log(event);
     let element = (event.target.tagName == "BUTTON") ? event.target : event.target.parentElement;
+
     let enabled = element.classList.contains("enabled");
-    if (enabled) {
-        // disable
+
+    if (element.hasAttribute("data-option")) {
+        let option = element.getAttribute("data-option");
+        browser.runtime.sendMessage({
+            setOption: {
+                key: option,
+                value: !enabled
+            }
+        }).then(response => {
+            if (response.success === true) {
+                element.classList.toggle("enabled");
+                console.log(`Sucessfully toggled ${option}!`);
+            } else {
+                console.log(`response.success !== true for ${option}, unexpected reponse:`);
+                console.log(response);
+            }
+        }, 
+        error =>{
+            console.log(`Something went wrong while trying to toggle ${option}:`);
+            console.log(error);
+        });
     } else {
-        // enable
+        // This shouldn't actually happen, it's here for testing.
+        // All options should have the data-option attribute,
+        // Otherwise, it might as well be a fidget spinner.
+        console.log(`Missing data-option attribute on:`);
+        console.log(element);
+        element.classList.toggle("enabled");
     }
-    element.classList.toggle("enabled"); // reimplement above
+}
+
+function updateAllOptions() {
+    browser.runtime.sendMessage({
+        getOptions: true
+    }).then(response => {
+        if(response.success !== true) {
+            console.log("response.success !== true when updating options, unexpected reponse: ");
+            console.log(response);
+            return;
+        } else if (!"options" in response) {
+            console.log("Options not provided when updating options, unexpected reponse: ");
+            console.log(response);
+            return;
+        }
+        response.options.forEach(option => {
+            let optionElement = document.querySelector(`[${option.key}]`);
+            if (option.value === true) {
+                optionElement.classList.add("enabled")
+            } else if (option.value === false) {
+                optionElement.classList.remove("enabled")
+            }
+        });
+    },
+    error => {
+        console.log(`Something went wrong when updating options:`)
+        console.log(error);
+    })
 }
 
 function localizePopupHTML() {
@@ -25,6 +76,7 @@ function localizePopupHTML() {
 
 document.addEventListener("DOMContentLoaded", () => {
     localizePopupHTML();
+    updateAllOptions();
     document.querySelectorAll("#settings>button").forEach(element => {
         element.addEventListener("click", optionToggled);
     });
