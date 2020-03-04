67e91be7969c2a8ad94fa2b109903126206f0618
PhaserArray <me@phaserarray.me>
2020-03-04 14:36:43 +0200

    onMessage handler testing

--- extension/background/moodle-controller.js
+++ extension/background/moodle-controller.js
@@ -1,5 +1,11 @@
-function onMessage(message, sender, sendResponse) {
-    
+function onMessage(message, sender) {
+    if (sender.url.endsWith("popup.html")) {
+        console.log("Message from popup script: ");
+        console.log(message);
+        return Promise.resolve({success: true});
+    } else {
+        console.log("Unknown origin!");
+    }
 }
 
 browser.runtime.onMessage.addListener(onMessage);
