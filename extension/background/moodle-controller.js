848d2b14de597d4320f6ec960f3061a144dc9044
PhaserArray <me@phaserarray.me>
2020-03-04 14:52:35 +0200

    update moodle-controller (WIP)

--- extension/background/moodle-controller.js
+++ extension/background/moodle-controller.js
@@ -2,7 +2,7 @@ function onMessage(message, sender) {
     if (sender.url.endsWith("popup.html")) {
         console.log("Message from popup script: ");
         console.log(message);
-        return Promise.resolve({success: true});
+        return Promise.resolve({success: true}); 
     } else {
         console.log("Unknown origin!");
     }
