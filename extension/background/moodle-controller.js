e399ce89dab6254e13727fb32a2721954e3d3656
PhaserArray <me@phaserarray.me>
2020-03-06 20:26:20 +0200

    Bulk of the background script, mostly related to the onMessage & getCurrentDomainOptions. Many other more minor things.

--- extension/background/moodle-controller.js
+++ extension/background/moodle-controller.js
@@ -1,8 +1,16 @@
-modules = ["singlePageBooks", "singlePageDicts", "midClickFix", "popupBlocker", "sessionKeepAlive"];
+// List of all modules and whether their default state.
+const modules = Object.freeze(
+                {"singlePageBooks": true, 
+                 "singlePageDicts": true, 
+                 "midClickFix": true, 
+                 "popupBlocker": true, 
+                 "sessionKeepAlive": true});
 
 // storage_sync_structure = {
 //     prefs: {
 //         "example.com": {
+//             domain: "example.com"
+//             moodle: true, 
 //             enabled: false,
 //             modules: {
 //                 list: true,
@@ -11,6 +19,8 @@ modules = ["singlePageBooks", "singlePageDicts", "midClickFix", "popupBlocker", 
 //             }
 //         },
 //         "school.moodledemo.net": {
+//             domain: "school.moodledemo.net"
+//             moodle: false, 
 //             enabled: true,
 //             modules: {
 //                 potentially: true,
@@ -23,16 +33,138 @@ modules = ["singlePageBooks", "singlePageDicts", "midClickFix", "popupBlocker", 
 //     }
 // };
 
-function onMessage(message, sender) {
-    if (typeof message === undefined) {
-        console.log("Received message with no actual message!");
-    }
-    if ("setModule" in message) {
-        // toggle module for current domain and respond
-    }
-    if ("setCurrentDomain" in message) {
-        // toggle module for current domain and respond
-    }
+function getDomainOptions(domain) {
+    return new Promise((resolve, reject) => {
+        browser.storage.local.get(domain)
+        .then(result => {
+            if (Object.entries(result).length === 0) {
+                return reject("not_found");
+            }
+            return resolve(result[domain]);
+        });
+    });
+}
+
+function getCurrentTab() {
+    return new Promise(resolve => {
+        browser.tabs.query({active: true, currentWindow: true})
+        .then(tabs => {
+            return resolve(tabs[0]);
+        });
+    });
+}
+
+function getHostnameFromURL(url) {
+    return (new URL(url)).hostname;
+}
+
+function getCurrentDomain() {
+    return new Promise(resolve => {
+        getCurrentTab().then(tab => {
+            resolve(getHostnameFromURL(tab.url));
+        });
+    });
+}
+
+function getCurrentDomainOptions() {
+    return new Promise((resolve, reject) => {
+        getCurrentDomain()
+        .then(domain => {
+            resolve(getDomainOptions(domain));
+        })
+        .catch(error => {
+            reject(error);
+        });
+    });
+}
+
+function setDomainOptions(domainOptions) {
+    return browser.storage.local.set({[domainOptions.domain]: domainOptions});
+}
+
+function isModulesCurrent(domainModules) {
+    let oldModuleKeys = Object.keys(domainModules).sort();
+    let newModuleKeys = Object.keys(modules).sort();
+    return JSON.stringify(oldModuleKeys) === JSON.stringify(newModuleKeys);
+}
+
+function updateModulesInOptions(options) {
+    let updatedModules = Object.assign({}, modules, options.modules);
+    options.modules = updatedModules;
+    return options;
+}
+
+function getDefaultOptions(domain) {
+    return {
+        domain: domain,
+        enabled: false,
+        moodle: null,
+        modules: {"singlePageBooks": false}
+    };
+}
+
+function messageSetModule(currentDomainOptions) {
+    return new Promise();
+}
+
+function messageSetCurrentDomain(currentDomainOptions) {
+    return new Promise();
+}
+
+function messageGetCurrentDomainOptions(currentDomainOptions) {
+    return new Promise(resolve => {
+        if (isModulesCurrent(currentDomainOptions.modules)) {
+            resolve({success: true, options:currentDomainOptions});
+        } else {
+            let updatedOptions = updateModulesInOptions(currentDomainOptions);
+            setDomainOptions(updatedOptions)
+            .then(() => {
+                resolve({success: true, options:updatedOptions});
+            });
+        }
+    });
+}
+
+function onMessage(message) {
+    return getCurrentDomainOptions()
+    .then(options => {
+        switch (true) {
+            case ("setModule" in message):
+                return messageSetModule(options);
+
+            case ("setCurrentDomain" in message):
+                return messageSetCurrentDomain(options);
+
+            case ("getCurrentDomainOptions" in message):
+                return messageGetCurrentDomainOptions(options);
+        
+            default:
+                return Promise.resolve({success:false, reason:"No valid message!"});
+        }
+    })
+    .catch(error => {
+        if (error !== "not_found") {
+            console.log("Unknown rejection when trying to process message: ");
+            console.log(error);
+            return Promise.resolve({success:false, reason:"Unknown reject!"});
+        }
+        return new Promise(resolve => {
+            getCurrentDomain()
+            .then(domain => {
+                console.log(domain);
+                let newDomainOptions = getDefaultOptions(domain);
+                setDomainOptions(newDomainOptions)
+                .then(() => {
+                    resolve(newDomainOptions);
+                })
+                .catch(error => {
+                    console.log("Failed to save new domain options: ");
+                    console.log(error);
+                    return Promise.resolve({success:false, reason:"Unable to save!"});
+                });
+            });
+        });
+    });
 }
 
 browser.runtime.onMessage.addListener(onMessage);
--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,7 +1,7 @@
-console.log("Popup script!")
+console.log("Popup script!");
 
 function buttonElementFromEvent(event) {
-    return (event.target.tagName == "BUTTON") ? event.target : event.target.parentElement;
+    return (event.target.tagName === "BUTTON") ? event.target : event.target.parentElement;
 }
 
 function isButtonOn(button) {
@@ -46,7 +46,7 @@ function optionToggled(event) {
 
 function updateAllOptions() {
     browser.runtime.sendMessage({
-        getOptions: true
+        getCurrentDomainOptions: true
     }).then(response => {
         if (typeof response === "undefined" 
             || !("success" in response) 
@@ -59,19 +59,40 @@ function updateAllOptions() {
             console.log(response);
             return;
         }
-        response.options.forEach(option => {
-            let optionElement = document.querySelector(`[${option.key}]`);
-            if (option.value === true) {
-                optionElement.classList.add("on")
-            } else if (option.value === false) {
-                optionElement.classList.remove("on")
+
+        let options = response.options;
+        let modules = options.modules;
+        Object.keys(modules).forEach(key => {
+            console.log(key);
+            let moduleElement = document.querySelector(`[data-module="${key}"]`);
+            if (typeof moduleElement !== "undefined") {
+                if (modules[key] === true) {
+                    moduleElement.classList.add("on");
+                } else if (modules[key] === false) {
+                    moduleElement.classList.remove("on");
+                }
             }
         });
+
+        let domainElement = document.querySelector("#state #toggle");
+        let settingsElement = document.querySelector("#settings");
+        if (options.enabled === true) {
+            domainElement.classList.add("on");
+            settingsElement.classList.remove("hidden");
+        } else if (options.enabled === false) {
+            domainElement.classList.remove("on");
+            settingsElement.classList.add("hidden");
+        } else if (options.moodle === false) {
+            domainElement.setAttribute("disabled", true);
+            domainElement.classList.remove("on");
+        }
+        domainElement.querySelector(".switch_on").textContent = options.domain;
+        domainElement.querySelector(".switch_off").textContent = options.domain;
     },
     error => {
-        console.log(`Something went wrong when updating options:`)
+        console.log(`Something went wrong when updating options:`);
         console.log(error);
-    })
+    });
 }
 
 function domainToggled(event) {
@@ -101,8 +122,6 @@ function domainToggled(event) {
 
 function localizePopupHTML() {
     document.querySelectorAll("[data-localize]").forEach(element => {
-        console.log(element.getAttribute("data-localize"));
-        console.log(browser.i18n.getMessage(element.getAttribute("data-localize")));
         element.innerHTML = browser.i18n.getMessage(element.getAttribute("data-localize"));
     });
     document.querySelectorAll("[data-localize-title]").forEach(element => {
