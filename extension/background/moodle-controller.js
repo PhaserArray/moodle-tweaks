37f844ea8b7a1e07510559ae0df08982bde0aa68
PhaserArray <me@phaserarray.me>
2020-03-06 14:04:08 +0200

    starting moodle-controller message handling

--- extension/background/moodle-controller.js
+++ extension/background/moodle-controller.js
@@ -1,10 +1,37 @@
+modules = ["singlePageBooks", "singlePageDicts", "midClickFix", "popupBlocker", "sessionKeepAlive"];
+
+// storage_sync_structure = {
+//     prefs: {
+//         "example.com": {
+//             enabled: false,
+//             modules: {
+//                 list: true,
+//                 of: false,
+//                 modules: true
+//             }
+//         },
+//         "school.moodledemo.net": {
+//             enabled: true,
+//             modules: {
+//                 potentially: true,
+//                 incomplete: true,
+//                 list: true,
+//                 of: false,
+//                 modules: true
+//             }
+//         }
+//     }
+// };
+
 function onMessage(message, sender) {
-    if (sender.url.endsWith("popup.html")) {
-        console.log("Message from popup script: ");
-        console.log(message);
-        return Promise.resolve({success: true}); 
-    } else {
-        console.log("Unknown origin!");
+    if (typeof message === undefined) {
+        console.log("Received message with no actual message!");
+    }
+    if ("setModule" in message) {
+        // toggle module for current domain and respond
+    }
+    if ("setCurrentDomain" in message) {
+        // toggle module for current domain and respond
     }
 }
 
