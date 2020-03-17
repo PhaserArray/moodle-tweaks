650a17695cba64a73533c6239460e93af67499eb
PhaserArray <me@phaserarray.me>
2020-03-17 16:41:05 +0200

    implement sessionKeepAlive

--- extension/content/modules/sessionKeepAlive.js
+++ extension/content/modules/sessionKeepAlive.js
@@ -1,1 +1,21 @@
 console.log("sessionKeepAlive.js");
+
+const keepAliveDelay = 10 * 60 * 1000; // 10 minutes * 60 seconds * 1000 milliseconds
+const keepAliveOn = true;
+
+const targetURL = document.querySelector(".navbar a.navbar-brand").getAttribute("href");
+if (targetURL !== null & targetURL !== "") {
+    keepAlive(targetURL);
+}
+
+function timeout(delay) {
+    return new Promise(resolve => setTimeout(resolve, delay));
+}
+
+async function keepAlive(url) {
+    while (keepAliveOn) {
+        await timeout(keepAliveDelay);
+        console.log(`Sending a request to ${targetURL} to keep the session alive.`);
+        await fetch(url, {credentials: "same-origin"});
+    }
+}
