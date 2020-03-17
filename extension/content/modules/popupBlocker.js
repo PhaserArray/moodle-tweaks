f869c7ee21c4688d9898a39ad54505940ddc0508
PhaserArray <me@phaserarray.me>
2020-03-17 18:20:11 +0200

    improve popupBlocker

--- extension/content/modules/popupBlocker.js
+++ extension/content/modules/popupBlocker.js
@@ -3,7 +3,24 @@ console.log("popupBlocker.js");
 var anchors = document.getElementsByTagName("a");
 for (const anchor of anchors) {
     if (!anchor.hasAttribute("onclick")) continue;
-    if (!anchor.onclick.toString().includes("&inpopup=1")) continue;
 
-    anchor.onclick = anchor.onclick.toString().replace("&inpopup=1", "");
+    let anchorOnclick = anchor.onclick.toString();
+
+
+    if (anchorOnclick.includes("&inpopup=1")) {
+        anchorOnclick = anchor.onclick.toString().replace("&inpopup=1", "");
+    }
+    if (anchorOnclick.includes("window.open(")) {
+        // I don't want to do the more expensive regex if it's not the right thing anyways.
+        // I also don't want to edit anything that might be something other than a popup link.
+        if (anchorOnclick.match(/window\.open\('.*?', '', 'width.*?'\); return false;/)) {
+            anchorOnclick = anchorOnclick.replace(/, 'width.*'/, "");
+        }
+    }
+
+    // Don't mess with it if it's the same string.
+    if (anchor.onclick.toString() !== anchorOnclick) {
+        console.log(anchorOnclick);
+        anchor.onclick = anchorOnclick;
+    }
 }
