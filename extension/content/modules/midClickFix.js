23a6f913013523d4d56526666b8b60b6fa8513a7
PhaserArray <me@phaserarray.me>
2020-03-17 18:05:31 +0200

    implement midClickFix

--- extension/content/modules/midClickFix.js
+++ extension/content/modules/midClickFix.js
@@ -1,1 +1,9 @@
 console.log("midClickFix.js");
+
+var anchors = document.getElementsByTagName("a");
+for (const anchor of anchors) {
+    if (!anchor.hasAttribute("onclick")) continue;
+    if (anchor.onclick.toString().includes("&redirect=1")) {
+        anchor.href += "&redirect=1";
+    }
+}
