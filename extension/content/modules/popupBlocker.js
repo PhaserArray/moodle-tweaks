aed41496194f2036a5df8cd0964558c9a95cc198
PhaserArray <me@phaserarray.me>
2020-03-17 17:52:57 +0200

    implement popupBlocker

--- extension/content/modules/popupBlocker.js
+++ extension/content/modules/popupBlocker.js
@@ -1,1 +1,9 @@
 console.log("popupBlocker.js");
+
+var anchors = document.getElementsByTagName("a");
+for (const anchor of anchors) {
+    if (!anchor.hasAttribute("onclick")) continue;
+    if (!anchor.onclick.toString().includes("&inpopup=1")) continue;
+
+    anchor.onclick = anchor.onclick.toString().replace("&inpopup=1", "");
+}
