287d33f53f228a0f983184e1f81096bf48984c0f
PhaserArray <me@phaserarray.me>
2020-03-17 23:03:09 +0200

    implement singlePageDicts

--- extension/content/modules/singlePageDicts.js
+++ extension/content/modules/singlePageDicts.js
@@ -1,1 +1,9 @@
 console.log("singlePageDicts.js");
+
+if (document.querySelector("body").id === "page-mod-glossary-view") {
+    const showAll = document.querySelector(".paging a[href*='page=-1']");
+    if (showAll !== null) {
+        showAll.click();
+    }
+    document.querySelectorAll(".paging").forEach(element => element.remove());
+}
