c24b83044e3984c1bde13740613f4d043bc875d5
PhaserArray <me@phaserarray.me>
2020-03-19 19:17:34 +0200

    fix url param appending

--- extension/content/modules/midClickFix.js
+++ extension/content/modules/midClickFix.js
@@ -4,6 +4,8 @@ var anchors = document.getElementsByTagName("a");
 for (const anchor of anchors) {
     if (!anchor.hasAttribute("onclick")) continue;
     if (anchor.onclick.toString().includes("&redirect=1")) {
-        anchor.href = new URL(anchor.href).searchParams.append("redirect", 1);
+        const url = new URL(anchor.href);
+        url.searchParams.append("redirect", "1");
+        anchor.href = url.href;
     }
 }
